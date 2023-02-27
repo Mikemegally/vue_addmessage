a63151e0f336d8a37c446af884a62e234cac0f39
michaelmegally <michaelgamil8@gmail.com>
2023-02-27 13:40:42 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,67 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <label for="message">write your message:</label><br>
+  <input type="tex" placeholder="enter your message" id="message" v-model=" content">
+  <button @click="addpost" >add</button>
+  <post v-for="post in posts" :key="post.content" :content="post.content"/>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import post from './components/post.vue'
+  export default {
+    name:'app', 
+    data(){
+        return{
+          content: '',
+          posts:[],
+        }
+    }, 
+    components:{
+      post
+    },
+    methods:{
+      addpost:function(){
+          this.posts.push({
+            content:this.content
+          })
+          this.content =''
+      }
+    }
   }
-}
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  input{
+    padding: 8px 10px;
+    width: 50%;
+    border: none;
+    background-color: brown;
+    color: white;
+    outline: none;
+    border-radius: 11px;
+  }
+  input::placeholder{
+    color: white;
+  }
+  button{
+    margin-left: 15px;
+    padding: 6px 25px;
+    text-transform: uppercase;
+    background-color: #a52a2a;
+    color: white;
+    border: 0;
+    border-radius: 10px;
+    cursor: pointer;
+    transition: all .3s;
+  }
+  button:hover{
+    background-color: darkblue;
+    transition: all .3s;
+    transform: scale(1.2);
+  }
+  label{
+    text-transform: capitalize;
+    font-weight: 600;
+    padding: 5px;
+  }
+  button,input{
+    margin-top: 10px;
+  }
 </style>
---
+++ src/components/post.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="post">
+        <p>
+           {{ content }}
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        // eslint-disable-next-line vue/multi-word-component-names
+        name:'post',
+        props:['content']
+    }
+</script>
+<style>
+    .post{
+        background-color: darkblue;
+        color: white;
+        padding: 20px;
+        margin-top: 20px;
+        border-radius: 15px;
+    }
+    .post p{
+        text-align: center;
+
+    }
+</style>
